ec2b2a57aa6a6fd2e488a96979bdadbd41a2356f
Diego Silva <contato@diegosilva.me>
2025-01-04 19:24:42 -0300

    changing and deleting files

--- app.js
+++
@@ -1,6 +0,0 @@
-import Server from "./src/server/Server.js";
-
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-const appServer = new Server(process.env.PORT);
--- run-failed-tests.sh
+++
@@ -1,2 +0,0 @@
-rm main.log
-NODE_OPTIONS='--experimental-vm-modules --experimental-specifier-resolution=node' jest -f
