d511d4c4f762f741b61324a370d6d65598f5bc5b
Diego Silva <contato@diegosilva.me>
2025-01-04 19:23:06 -0300

    Added pallete

--- frontend/src/services/queryService.ts
+++ frontend/src/services/queryService.ts
@@ -1,7 +1,9 @@
 import api from './api';
 import Result from '@/types/Result';
 
-export async function runQuery(query: String) : Promise<Result> {
-  const response = await api.get(`/expression/${query}`);
+export async function runQuery(query: string) : Promise<Result> {
+  const encodedQuery = encodeURIComponent(query);
+  console.log(encodedQuery);
+  const response = await api.get(`/expression/${encodedQuery}`);
   return response.data;
 };
